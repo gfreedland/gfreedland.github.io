e26edb4735d551ef4c7b1b80d30d6036e4e25daa
g.freedland <gfreedland@mail.sfsu.edu>
2021-02-12 12:28:20 -0800

    nav animation added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelector('.nav-links li');
 
   burger.addEventListener('click', () => {
+    // Toggle the nav bar
     nav.classList.toggle('nav-active');
+    // Animate the links
+    navLinks.forEach((item, index) => {
+      if (item.style.animation) item.style.animation = '';
+      else {
+        item.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 5 + 1
+        }s`;
+      }
+    });
   });
 };
 
